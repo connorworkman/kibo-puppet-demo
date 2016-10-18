29de4c6d6f9900567668cff018d08fca82000614
Connor Workman <connor.workman@kibocommerce.com>
2016-10-18 13:38:39 -0500

    Added file references within an if statement.

--- modules/demo-module/manifests/init.pp
+++ modules/demo-module/manifests/init.pp
@@ -4,21 +4,17 @@ class demo-module {
         content => "This was created by Puppet Master version ${serverversion}.",
         path    => '/home/ec2-user/puppet-demo.txt',
     }
-    if $operatingsystem == 'CentOS' {
+    if $operatingsystem == 'Amazon' {
         notify{"Your OS is ${operatingsystem}.":}
-        notice("Your OS is bad.")
-        file { 'puppet-demo.csv':
-            ensure  => file,
-            content => "hit if statement",
-            path    => '/home/ec2-user/puppet-demo.csv',
-        }
+        $source = 'puppet:///modules/demo-module/puppet-demo2.csv',
     } else {
         notify{"This operating system is ${operatingsystem}.":}
-        notice("Your OS is good.")
-        file { 'puppet-demo2.csv':
-            ensure  => file,
-            content => "hit else statement",
-            path    => '/home/ec2-user/puppet-demo2.csv',
-        }
+        notice("Your OS is definitely not Amazon.")
+        $source = 'puppet:///modules/demo-module/puppet-demo.csv',
     }
+        file { 'puppet-demo.csv':
+            ensure  => file,
+            path    => '/home/ec2-user/puppet-demo.csv',
+            source  => $source,
+        }
 }

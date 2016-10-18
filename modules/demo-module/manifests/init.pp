6f5d2f103f7bf322a61352047e703a12e319693f
Connor Workman <connor.workman@kibocommerce.com>
2016-10-18 11:58:09 -0500

    Testing out warning comments in demo-module

--- modules/demo-module/manifests/init.pp
+++ modules/demo-module/manifests/init.pp
@@ -4,4 +4,9 @@ class demo-module {
         content => "This was created by Puppet Master version ${serverversion}.",
         path    => '/home/ec2-user/puppet-demo.txt',
     }
+    if $operatingsystem == 'CentOS' {
+        warning
+    } else {
+        warning("This operating system is ${operatingsystem}.")
+    }
 }

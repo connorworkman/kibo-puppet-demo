388a04824a0274fbe73109df2fb1668e75558c19
Connor Workman <connor.workman@kibocommerce.com>
2016-10-18 11:33:44 -0500

    Path attribute correction on demo module

--- modules/demoModule/manifests/init.pp
+++ modules/demoModule/manifests/init.pp
@@ -2,6 +2,6 @@ class demomodule {
     file { 'puppet-demo.txt':
         ensure  => file,
         content => 'This was created by Puppet Master version ${serverversion}.',
-        path    => '/home/ec2-user/',
+        path    => '/home/ec2-user/puppet-demo.txt',
     }
 }

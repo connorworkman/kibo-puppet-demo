c0b9d9b9191a846ddf56a6fd259f792b9d861f24
Connor Workman <connor.workman@kibocommerce.com>
2016-10-18 11:01:06 -0500

    Forgot class declaration in demoModule manifest

--- modules/demoModule/manifests/init.pp
+++ modules/demoModule/manifests/init.pp
@@ -1,5 +1,7 @@
-file { 'puppet-demo.txt':
-    ensure  => file,
-    content => 'This was created by Puppet Master version ${serverversion}.',
-    path    => '/home/ec2-user/',
+class demoModule {
+    file { 'puppet-demo.txt':
+        ensure  => file,
+        content => 'This was created by Puppet Master version ${serverversion}.',
+        path    => '/home/ec2-user/',
+    }
 }

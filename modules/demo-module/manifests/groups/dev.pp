a6892a1f164bc32ebb8ed2f6755b742fffc048bd
Connor Workman <connor.workman@kibocommerce.com>
2016-10-19 14:59:18 -0500

    Adding some more advanced module features

---
+++ modules/demo-module/manifests/groups/dev.pp
@@ -0,0 +1,5 @@
+class demo-modules::groups::dev {
+    group { 'dev':
+        ensure => present,
+    }
+}
--- modules/demo-module/manifests/init.pp
+++ modules/demo-module/manifests/init.pp
@@ -1,26 +1,37 @@
 class demo-module {
+    #switch this if to case statement and continue on with linuxacademy tutorials
+    user { 'kibo-dev':
+        ensure      => present,
+        home        => '/home/kibo-user',
+        gid         => 'dev',
+        shell       => '/bin/bash',
+        managehome  => true,
+    }
+    user { 'kibo-admin':
+        ensure      => present,
+        managehome  => false,
+        gid         => 'wheel',
+        shell       => '/bin/bash',
+    }
     if $operatingsystem == 'Amazon' {
         notify{"Your OS is ${operatingsystem}.":}
         $source = 'puppet:///modules/demo-module/puppet-demo2.csv'
-        $username = 'ec2-user'
     } elsif $operatingsystem == 'RedHat' {
         notify{"Your OS is RHEL.":}
         $source = 'puppet:///modules/demo-module/puppet-demo2.csv'
-        $username = 'ec2-user'
     } else {
         notify{"This operating system is ${operatingsystem}.":}
         notice("Your OS is definitely not Amazon.")
         $source = 'puppet:///modules/demo-module/puppet-demo.csv'
-        $username = 'user'
     }
     file { 'puppet-demo.csv':
-        ensure  => file,
-        path    => "/home/${username}/puppet-demo.csv",
-        source  => $source,
+        ensure      => file,
+        path        => "/home/kibo-user/puppet-demo.csv",
+        source      => $source,
     }
     file { 'puppet-demo.txt':
-        ensure  => file,
-        content => "This was created by Puppet Master version ${serverversion}.",
-        path    => "/home/${username}/puppet-demo.txt",
+        ensure      => file,
+        content     => "This was created by Puppet Master version ${serverversion}.",
+        path        => "/home/kibo-user/puppet-demo.txt",
     }
 }

b066aee2314b14f074790da7b00205119d999f23
Connor Workman <connor.workman@kibocommerce.com>
2016-10-19 12:31:21 -0500

    Added conditional check as elif branch

--- modules/demo-module/manifests/init.pp
+++ modules/demo-module/manifests/init.pp
@@ -4,11 +4,16 @@ class demo-module {
         content => "This was created by Puppet Master version ${serverversion}.",
         path    => '/home/ec2-user/puppet-demo.txt',
     }
-    if (($operatingsystem == 'Amazon') || ($operatingsystem == 'Red Hat Enterprise Linux Server'))  {
+    if $operatingsystem == 'Amazon' {
         notify{"Your OS is ${operatingsystem}.":}
         $source = 'puppet:///modules/demo-module/puppet-demo2.csv'
         $username = 'ec2-user'
-    } else {
+    } elif $operatingsystem == 'Red Hat Enterprise Linux Server' {
+        notify{"Your OS is RHEL.":}
+        $source = 'puppet:///modules/demo-module/puppet-demo2.csv'
+        $username = 'ec2-user'
+    } 
+    else {
         notify{"This operating system is ${operatingsystem}.":}
         notice("Your OS is definitely not Amazon.")
         $source = 'puppet:///modules/demo-module/puppet-demo.csv'
